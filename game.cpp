ac439796c382ad70e741947ac15beb7ec4201388
irumvag <irumvag@kalizero>
2024-11-08 00:31:58 +0200

    This tic tac toe in cpp langauge

--- game.cpp
+++ game.cpp
@@ -29,13 +29,68 @@ void display0()
     }
 class playing{
     private:
-    char a[9];
-    char b[9];
+    int a[9]={0},b[9]={0};
+    char c[9];
     public:
+    int p2,p1;
     int s;
-    char p;
     string name1,name2;
-    void players(){
+    void players();
+    void input1(){
+        bool t,f;
+        int ic;
+        cout<<"\n\n  Enter position #"<<name1<<" you want to play from 1-9:";
+        cin>>p1;
+        if(p1>0&&p1<10)
+        {
+            t=(a[p1-1]!=0);
+            f=(b[p1-1]!=0);
+            if(t||f){
+                ic=1;
+                cout<<"\nThe space is already accupied????\n";
+                input1();
+            }
+            else{
+                c[p1-1]='X';
+                a[p1-1]=p1;
+                cout<<"\nNext:\n"; 
+            }
+        }
+        else{
+            cout<<"\nInvalid value!\n";
+            input1();
+        }
+    }
+    void input2(){
+        bool t,f;
+        int ic;
+        cout<<"\n\n  Enter position #"<<name2<<" you want to play from 1-9:";
+        cin>>p2;
+        if(p2>0&&p2<10)
+        {
+            t=a[p2-1]!=0;
+            f=b[p2-1]!=0;
+            if(t||f){
+                ic=1;
+                cout<<"\nThe space is already accupied????\n";
+                input1();
+            }
+            else{
+                c[p2-1]='O';
+                b[p2-1]=p2;
+                cout<<"\nNext:\n"; 
+            }
+        }
+        else{
+            cout<<"\nInvalid value!\n";
+            input1();
+        }
+    }
+    void update(){
+        
+    }
+};
+void playing::players(){
     cout<<"\nYou are most welcome in TIC-TAC-TOE Game!!!!\n\n";
     display0();
     cout<<"Enter first player name:";
@@ -48,56 +103,13 @@ class playing{
     cout<<"   Second player is represented by 'O':"<<name1<<endl<<endl;
     display0();
     }
-    void input1(char p1){
-        cout<<"\n\n  Enter position #"<<name1<<" you want to play from 1-9:";
-        cin>>p;
-        for(int c=0;c<9;c++)
-        {
-            if((p1==a[c]||p1==b[c])){
-                cout<<"Value is already allocated try again:";
-                break;
-            }
-            else
-            {
-             s=int(p1);
-             a[s-1]=p1;
-             cout<<"continue....";   
-             break;
-            }
-        }
-    }
-    void input2(char p2){
-        cout<<"\n\n  Enter position #"<<name2<<"you want to play from 1-9:";
-        cin>>p;
-        for(int c=0;c<9;c++)
-        {
-            if(p2==b[c]||p2==a[c]){
-                cout<<"Value is already allocated try again:";
-                break;
-            }
-            else
-            {
-             s=int(p2);
-             b[s-1]=p2;
-             cout<<"continue....";   
-             break;
-            }
-        }
-    }
-    void update(){
-
-    }
-};
 int main(){
-    int count[9];
-    char x='X',o='O';
-    char p;
     playing p1;
     p1.players();
     for(int i=1;i<10000;i++){
-    p1.input1(p);
+    p1.input1();
     p1.update();
-    p1.input2(p);
+    p1.input2();
     p1.update();
     }
     return 0;
